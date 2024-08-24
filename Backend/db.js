ef6b24be98b2dd962d8a9dabdfba5fc3b3e6c62a
MuzdalfaZulfiqar <Muzdalfazulfiqar11@gmail.com>
2024-08-24 21:02:43 +0500

    connected to atlas

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,19 @@
-const mongoose = require("mongoose")
-const mongoURI  = "mongodb://localhost:27017/RecipeMaster"
+const mongoose = require("mongoose");
 
-function connectToMongo(){
-    mongoose.connect(mongoURI).
-    then(()=>{console.log("Connected to mongo DB Successfully")}).
-    catch((error)=>{console.log(error)})
+// Replace with your MongoDB Atlas connection string
+const mongoURI = "mongodb+srv://muzdalfazulfiqar11:Lh0zZKaUxoKE8lsf@cluster0.8dl8o.mongodb.net/RecipeMaster?retryWrites=true&w=majority";
+
+function connectToMongo() {
+    mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB Atlas Successfully");
+    })
+    .catch((error) => {
+        console.log("Error connecting to MongoDB Atlas:", error);
+    });
 }
 
-module.exports = connectToMongo
+module.exports = connectToMongo;
