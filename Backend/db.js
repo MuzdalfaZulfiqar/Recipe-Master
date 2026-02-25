a53beb53520a11f7bb2c031ba9b0aff26bb53da1
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2026-02-25 17:00:03 +0500

    Addedbakcend

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,25 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-// Replace with your MongoDB Atlas connection string
-const mongoURI = "mongodb+srv://muzdalfazulfiqar11:Lh0zZKaUxoKE8lsf@cluster0.8dl8o.mongodb.net/RecipeMaster";
+// // Replace with your MongoDB Atlas connection string
+// const mongoURI = "mongodb+srv://muzdalfazulfiqar11:Lh0zZKaUxoKE8lsf@cluster0.8dl8o.mongodb.net/RecipeMaster";
+
+// function connectToMongo() {
+//     mongoose.connect(mongoURI)
+//     .then(() => {
+//         console.log("Connected to MongoDB Atlas Successfully");
+//     })
+//     .catch((error) => {
+//         console.log("Error connecting to MongoDB Atlas:", error);
+//     });
+// }
+
+// module.exports = connectToMongo;
+
+
+const mongoose = require("mongoose");
+require('dotenv').config(); // load env variables
+
+const mongoURI = process.env.MONGO_URI;
 
 function connectToMongo() {
     mongoose.connect(mongoURI)
@@ -14,4 +32,3 @@ function connectToMongo() {
 }
 
 module.exports = connectToMongo;
-
